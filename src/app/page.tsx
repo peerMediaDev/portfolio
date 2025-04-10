9172b571a98b6ac7dd45a8c39a6dfb7bd3725efc
peermediadev <mason.caird@peermedia.co>
2025-04-10 15:09:12 -0700

    fix: fix building issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -7,7 +5,7 @@ export default function Home() {
         <h1 className="text-black font-extrabold text-4xl sm:text-5xl leading-tight">
           Hi, Welcome to Peer Media LLC.
         </h1>
-        <p>We're currently under maintenance, but we'll be right back soon.</p>
+        <p>We&apos;re currently under maintenance, but we&apos;ll be right back soon.</p>
         <p>Stay tuned for a much cooler website experience coming your way!</p>
 
       </main>
